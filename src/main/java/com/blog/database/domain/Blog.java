56c528e0a21fde0e6e17eda2bc92294d743c563d
Sergii Manko <sergii.manko@gmail.com>
2017-10-07 22:27:49 +0300

    Fixed bug: incorrect field in class Blog

--- src/main/java/com/blog/database/domain/Blog.java
+++ src/main/java/com/blog/database/domain/Blog.java
@@ -1,6 +1,9 @@
 package com.blog.database.domain;
 
-import lombok.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import javax.persistence.*;
 
@@ -18,20 +21,8 @@ public class Blog {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(nullable = false, updatable = false)
     private Long id;
-
     @Column(nullable = false)
     private String title;
-
     @Column(nullable = false)
     private String body;
-
-    @Column(nullable = false)
-    private Data data;
-
-
-    public Blog(String title, String body, Data data) {
-        this.title = title;
-        this.body = body;
-        this.data = data;
-    }
 }
